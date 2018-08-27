69e170108d732dab10b78c4ab3075e6169ac4546
Thomas Kapp <thomaskpp@gmail.com>
2018-08-27 15:35:12 -0300

    criar AppService e Repository Client

---
+++ BecaDotNet.ApplicationService/ClientAppService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using BecaDotNet.Domain.Model;
+using BecaDotNet.Domain.Service;
+using BecaDotNet.Repository;
+
+namespace BecaDotNet.ApplicationService
+{
+    public class ClientAppService : IGenericService<Client>
+    {
+        ClientRepository rep = new ClientRepository();
+
+        public Client Create(Client toCreate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Client> FindBy(Client filter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Client Get(int id)
+        {
+            var result = rep.GetSingle(id);
+            return result;
+        }
+
+        public Client Update(Client toUpdate)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BecaDotNet.Repository/ClientRepository.cs
@@ -0,0 +1,9 @@
+﻿using BecaDotNet.Domain.Model;
+using BecaDotNet.Repository.Context;
+
+namespace BecaDotNet.Repository
+{
+    public class ClientRepository : GenericRepository<BecaContext, Client>
+    {
+    }
+}
