3419c90e498c14b1b9340bd7f71224230c3eb6b8
Thomas Kapp <thomaskpp@gmail.com>
2018-08-28 14:15:45 -0300

    ddlHelper

--- BecaDotNet.UI.MVC.RazorView/Models/HelperForDropdown.cs
+++ BecaDotNet.UI.MVC.RazorView/Models/HelperForDropdown.cs
@@ -6,9 +6,9 @@ namespace BecaDotNet.UI.MVC.RazorView.Models
 {
     public static class HelperForDropdown<T> where T : IdentifiedEntity
     {
-        public static IEnumerable<SelectListItem> GetDropDownListFrom(IEnumerable<T> list, string propName)
+        public static IEnumerable<SelectListItem> GetDropDownListFrom(IEnumerable<T> list, string propName, string defaultText = "Selecione")
         {
-            var result = new List<SelectListItem> { new SelectListItem { Text="Selecione", Value="-1"} };
+            var result = new List<SelectListItem> { new SelectListItem { Text=defaultText, Value="-1"} };
             foreach (var i in list)
                 result.Add(new SelectListItem
                 {
